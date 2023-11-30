e102ef8e505a0e2c3b200e3180ace4d55f813c15
ritika-saha <ritika882001@gmail.com>
2023-11-30 20:14:49 +0530

    firstname Validation

--- src/main/java/com/user/UserRegMain.java
+++ src/main/java/com/user/UserRegMain.java
@@ -1,7 +1,18 @@
 package com.user;
+import java.util.Scanner;
 
 public class UserRegMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to User Registration Program");
+        Scanner sc=new Scanner(System.in);
+
+        UserRegTest testData=new UserRegTest();
+
+        //Validating first name
+        System.out.println("Enter First Name");
+        String fname=sc.nextLine();
+        boolean isValid=testData.validateFirstName(fname);
+        System.out.println(isValid?"First Name - VALID":"First Name -  INVALID");
+        
     }
 }
---
+++ src/main/java/com/user/UserRegTest.java
@@ -0,0 +1,15 @@
+package com.user;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegTest {
+
+    public boolean validateFirstName(String data){
+        final String regex = "^[A-Z][a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(data);
+        boolean result=matcher.matches();
+        return result;
+    }
+    
+}
