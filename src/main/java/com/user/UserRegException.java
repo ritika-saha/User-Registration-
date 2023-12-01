b70625d83446167f680b6d1227a8b60af1a8167a
ritika-saha <ritika882001@gmail.com>
2023-12-02 02:23:08 +0530

    clean up

--- src/main/java/com/user/UserRegException.java
+++ src/main/java/com/user/UserRegException.java
@@ -1,16 +1,7 @@
 package com.user;
 
 public class UserRegException extends Exception {
-    //public MoodAnalyserErr err;
-//
-    //public MoodAnalyserException(MoodAnalyserErr e) {
-    //    super(e.getMessage());
-    //    this.err = e;
-    //}
-//
-    //public MoodAnalyserErr getError(){
-    //    return err;
-    //}
+  
 
     String message;
     public UserRegException(String s){
